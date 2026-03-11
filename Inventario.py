072df231680703cdaf9ba2675c279adac85fbd96
jhonfredy2710-bit <jhonfredy2710@gmail.com>
2026-03-10 19:05:35 -0500

    Add files via upload

---
+++ Inventario.py
@@ -0,0 +1,11 @@
+#inventario tienda el dolar
+products = input("into name of a product: ")
+price = float(input("into price of product: "))
+quantity = int(input("into quantity: "))
+resultado = price * quantity
+print(f"{products} , {price} * {quantity} = {resultado}  ")
+if price == input():
+    print(f"into again price: ")
+
+elif quantity == input():
+    print(f"into again quantity: ") 
